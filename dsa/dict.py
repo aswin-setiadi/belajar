b1760484c129100c1814e598a90b3c0b38d5e9a8
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-02 19:14:59 +0800

    trivial commit

---
+++ dsa/dict.py
@@ -0,0 +1,10 @@
+def play_dict():
+    d = {"a": {"s": {"w": {"i": {"n": {}}}}}, "b": 2}
+    if "a" in d:
+        print(True)
+    else:
+        print(False)
+
+
+if __name__ == "__main__":
+    play_dict()
