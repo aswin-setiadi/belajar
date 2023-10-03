10460732973801a897f2a509cc3aaa2db1ce0ff8
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-03 15:08:54 +0800

    add custom exceptions

--- schroders/custom_exceptions.py
+++ schroders/custom_exceptions.py
@@ -1,6 +1,21 @@
 """Custom Exceptions Module"""
 
 
+class DuplicateKeyException(Exception):
+    def __init__(self) -> None:
+        super().__init__(f"Duplicate key found.")
+
+
 class InvalidKeyPadLayoutException(Exception):
     def __init__(self) -> None:
-        super().__init__(f"Keypad layout is invalid, minimum size is 2x3 or 3x2")
+        super().__init__(f"Keypad layout is invalid, minimum size is 2x3 or 3x2.")
+
+
+class InvalidMaxDepthException(Exception):
+    def __init__(self) -> None:
+        super().__init__(f"max_depth can't be less than 0.")
+
+
+class InvalidMaxVowelException(Exception):
+    def __init__(self) -> None:
+        super().__init__(f"max_vowel can't be less than 0.")
