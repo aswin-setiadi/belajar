914c16c0b3f494e94abe7420bbf5d8cc4d73f8c1
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-03 14:01:11 +0800

    update test for dynamic map

--- schroders/test.py
+++ schroders/test.py
@@ -3,8 +3,16 @@ from main import Keypad
 
 
 class TestKeypad(unittest.TestCase):
-    def test_output(self):
-        self.assertEqual(Keypad().solve(), 1013398)
+    def test_sample_input(self):
+        matrix: list[list[str | None]] = [
+            ["A", "B", "C", "D", "E"],
+            ["F", "G", "H", "I", "J"],
+            ["K", "L", "M", "N", "O"],
+            [None, "1", "2", "3", None],
+        ]
+        self.assertEqual(
+            Keypad(matrix=matrix, max_depth=10, max_vowel=2).solve(), 1013398
+        )
 
 
 if __name__ == "__main__":
