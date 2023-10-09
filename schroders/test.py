68c42333d971918c4eb0edc425b221cd5838fe00
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-09 18:24:02 +0800

    update test cases

--- schroders/test.py
+++ schroders/test.py
@@ -1,5 +1,6 @@
 import unittest
-from main import Solution
+from main import Solution, Builder
+from global_vars import VALID_PIECES
 from custom_exceptions import (
     DuplicateKeyException,
     InvalidKeyPadLayoutException,
@@ -17,18 +18,36 @@ class TestKeypad(unittest.TestCase):
             [None, "1", "2", "3", None],
         ]
         self.assertEqual(
-            Solution.solve(matrix=matrix, max_depth=10, max_vowel=2), 1013398
+            Solution.solve(
+                matrix=matrix, max_depth=10, max_vowel=2, move_type=VALID_PIECES[0]
+            ),
+            1_013_398,
         )
 
-    def test_valid_matrix(self):
+    def test_valid_matrix_knight(self):
         matrix1 = [["A", "B", "C"], ["D", "E", "F"]]
         matrix2 = [["A", "B", None], ["D", "E", "F"]]
         matrix3 = [[None, "B", None], ["D", "E", "F"]]
-        self.assertEqual(Solution.solve(matrix=matrix1, max_depth=3, max_vowel=3), 4)
-        self.assertEqual(Solution.solve(matrix=matrix2, max_depth=3, max_vowel=3), 2)
-        self.assertEqual(Solution.solve(matrix=matrix3, max_depth=3, max_vowel=3), 0)
+        self.assertEqual(
+            Solution.solve(
+                matrix=matrix1, max_depth=3, max_vowel=3, move_type=VALID_PIECES[0]
+            ),
+            4,
+        )
+        self.assertEqual(
+            Solution.solve(
+                matrix=matrix2, max_depth=3, max_vowel=3, move_type=VALID_PIECES[0]
+            ),
+            2,
+        )
+        self.assertEqual(
+            Solution.solve(
+                matrix=matrix3, max_depth=3, max_vowel=3, move_type=VALID_PIECES[0]
+            ),
+            0,
+        )
 
-    def test_invalid_matrix(self):
+    def test_invalid_matrix_knight(self):
         matrixes = []
         matrixes.append([[]])
         matrixes.append([["A"], ["B"]])
@@ -37,22 +56,36 @@ class TestKeypad(unittest.TestCase):
         matrixes.append([["A", "B", "C"], ["D", "E", "F"], ["G", None]])
         for matrix in matrixes:
             with self.assertRaises(InvalidKeyPadLayoutException):
-                Solution.solve(matrix=matrix, max_depth=10, max_vowel=2)
+                Solution.solve(
+                    matrix=matrix, max_depth=10, max_vowel=2, move_type=VALID_PIECES[0]
+                )
 
     def test_duplicate_keys(self):
         matrix = [["A", "B", "C"], ["A", "E", "F"]]
         with self.assertRaises(DuplicateKeyException):
-            Solution.solve(matrix=matrix, max_depth=10, max_vowel=2)
+            Solution.solve(
+                matrix=matrix, max_depth=10, max_vowel=2, move_type=VALID_PIECES[0]
+            )
 
     def test_invalid_max_depth(self):
         matrix = [["A", "B", "C"], ["D", "E", "F"]]
         with self.assertRaises(InvalidMaxDepthException):
-            Solution.solve(matrix=matrix, max_depth=-1, max_vowel=2)
+            Solution.solve(
+                matrix=matrix, max_depth=-1, max_vowel=2, move_type=VALID_PIECES[0]
+            )
 
     def test_invalid_max_vowel(self):
         matrix = [["A", "B", "C"], ["D", "E", "F"]]
         with self.assertRaises(InvalidMaxVowelException):
-            Solution.solve(matrix=matrix, max_depth=10, max_vowel=-1)
+            Solution.solve(
+                matrix=matrix, max_depth=10, max_vowel=-1, move_type=VALID_PIECES[0]
+            )
+
+    def test_rook_move(self):
+        matrix = [["A", "B"], ["D", "E"], [None, None]]
+        out_adj = Builder.build_adjacency_dict(matrix, "rook")
+        exp_adj = {"A": ["B", "D"], "B": ["A", "E"], "D": ["E", "A"], "E": ["D", "B"]}
+        self.assertEqual(out_adj, exp_adj)
 
 
 if __name__ == "__main__":
