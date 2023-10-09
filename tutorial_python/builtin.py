c57ad003885a3bdb6637cb2fe69425d339f5ce96
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-09 23:34:08 +0800

    update builtin ds / function notes

--- tutorial_python/builtin.py
+++ tutorial_python/builtin.py
@@ -1,18 +1,33 @@
+from collections import deque
 from functools import reduce
 
-fruits = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
 
 
-def starts_with_A(s):
-    if s[0] == "A":
-        return s  # or True, so will be True, None, etc.
-    # auto else to None, so must declare
+
+def main():
+    def starts_with_A(s: str):
+        if s[0] == "A":
+            return s  # or True, so will be True, None, etc.
+        # auto else to None, so must declare
+
+    fruits = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+    map_object = map(starts_with_A, fruits)
+    print(list(map_object))
+
+    filter_object = filter(lambda x: x[0] == "A", fruits)
+    print(list(filter_object))
+
+    print(reduce(lambda x, y: x + y, fruits))
 
 
-map_object = map(starts_with_A, fruits)
-print(list(map_object))
+q = deque()
+q.append("a")
+q.append("b")
+q.append("c")
+print(q.popleft())
+print(q.)
 
-filter_object = filter(lambda x: x[0] == "A", fruits)
-print(list(filter_object))
-
-print(reduce(lambda x, y: x + y, fruits))
+#for stack append and pop, deque is faster than list
+# when python need to reallocate memory to grow underlying list, the operation are slower and can become O(n)
+# python set is unordered and after created, element cant be changed but can add/ remove and must unique
+# tuple is ordered and element is unchangeable and can't add/ remove
