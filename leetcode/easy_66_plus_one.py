285af0c812d9e5264b4d0ec5de28374c963cbc57
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-10 16:16:23 +0800

    easy 66 plus one

---
+++ leetcode/easy_66_plus_one.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        arr = digits[::-1]
+        carry = 1
+        for i in range(len(arr)):
+            d_sum = arr[i] + carry
+            arr[i] = d_sum % 10
+            carry = d_sum // 10
+            if i == len(arr) - 1:
+                if carry != 0:
+                    arr.append(carry)
+        return arr[::-1]
+
+
+# [1,2] will return [1,3], [9] will return [1,0]
