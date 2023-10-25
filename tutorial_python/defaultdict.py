1fc361a39f6f354f9b3e9611d8443408f19f5041
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-25 15:01:42 +0800

    update tutorial defauldict

--- tutorial_python/defaultdict.py
+++ tutorial_python/defaultdict.py
@@ -5,19 +5,30 @@
 from collections import defaultdict
 
 
-# Function to return a default
-# values for keys that is not
-# present
-def def_value():
-    return "Not Present"
+def main():
+    # Function to return a default
+    # values for keys that is not
+    # present
+    def def_value():
+        return "Not Present"
+
+    # Defining the dict
+    d = defaultdict(def_value)
+    d["a"] = 1
+    d["b"] = 2
+    print(d)
+    print(d["a"])
+    print(d["b"])
+    print(d["c"])
+    print(def_value)
 
 
-# Defining the dict
-d = defaultdict(def_value)
-d["a"] = 1
-d["b"] = 2
+def main2():
+    d = defaultdict(dict)
+    d["b"]["b"] = 1
+    print(d)
 
-print(d["a"])
-print(d["b"])
-print(d["c"])
-print(def_value)
+
+if __name__ == "__main__":
+    main()
+    main2()
