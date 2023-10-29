6bf8b027db960e68ce425b1596caa3e97f86552b
aswin-setiadi <aswin.setiadi@gmail.com>
2023-10-30 00:37:54 +0800

    update timeit info

--- tutorial_python/fn.py
+++ tutorial_python/fn.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 def outer(l: list[int]):
     print("out")
 
@@ -9,4 +12,8 @@ def outer(l: list[int]):
 
 
 v = outer([1])
+print(v)  # inner function
 v()  # will keep l ref hence when called still can print 1
+# number= count of timer call, repeat= count of timeit call
+ans = min(timeit.repeat(outer([1, 2]), repeat=2, number=3))
+print(ans)
