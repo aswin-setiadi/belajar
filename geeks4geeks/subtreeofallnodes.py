af149bd606d6b9aab4ae21d7fd34320ad12f9975
aswin-setiadi <aswin.setiadi@gmail.com>
2023-11-09 18:41:30 +0800

    refactor g4g code

--- geeks4geeks/subtreeofallnodes.py
+++ geeks4geeks/subtreeofallnodes.py
@@ -3,75 +3,74 @@
 
 from typing import List
 
-# Python3 code to print subtree of all nodes
 
-# arrays for keeping position at
-# each dfs traversal for each node
-start: List[int] = [0] * 100001
-end: List[int] = [0] * 100001
+class GeeksforGeeks:
+    def __init__(self) -> None:
+        # Python3 code to print subtree of all nodes
 
-# Storing dfs order
-dfs_order: List[int] = []
-adj: List[List[int]] = [[] for _ in range(100001)]
-visited = [False] * 100001
+        # arrays for keeping position at
+        # each dfs traversal for each node
+        self.start: List[int] = [0] * 100001
+        self.end: List[int] = [0] * 100001
 
-# Recursive function for dfs traversal dfsUtil()
+        # Storing dfs order
+        self.dfs_order: List[int] = []
+        self.adj: List[List[int]] = [[] for _ in range(100001)]
+        self.visited = [False] * 100001
 
+    # Recursive function for dfs traversal dfsUtil()
+    def dfs(self, a: int, b: int) -> int:
+        """
+        a: starting node, usually root
+        b:
+        """
+        # keep track of node visited
+        self.visited[a] = True
+        b += 1
+        self.start[a] = b
+        self.dfs_order.append(a)
 
-def dfs(a: int, b: int) -> int:
-    """
-    a: starting node, usually root
-    b:
-    """
-    # keep track of node visited
-    visited[a] = True
-    b += 1
-    start[a] = b
-    dfs_order.append(a)
+        for it in self.adj[a]:
+            if not self.visited[it]:
+                b = self.dfs(it, b)
 
-    for it in adj[a]:
-        if not visited[it]:
-            b = dfs(it, b)
+        self.end[a] = b
+        return b
 
-    end[a] = b
-    return b
+    # Function to print the subtree nodes
 
+    def Print(self, n: int):
+        for i in range(0, n):
+            # If node is leaf node
+            # start[i] is equals to endd[i]
+            if self.start[i] != self.end[i]:
+                print("subtree of node", i, "is", end=" ")
+                for j in range(self.start[i] + 1, self.end[i] + 1):
+                    print(self.dfs_order[j - 1], end=" ")
 
-# Function to print the subtree nodes
-
-
-def Print(n: int):
-    for i in range(0, n):
-        # If node is leaf node
-        # start[i] is equals to endd[i]
-        if start[i] != end[i]:
-            print("subtree of node", i, "is", end=" ")
-            for j in range(start[i] + 1, end[i] + 1):
-                print(dfs_order[j - 1], end=" ")
-
-            print()
+                print()
 
 
 # Driver code
 if __name__ == "__main__":
     # No of nodes n = 10
     n, c = 10, 0
-
-    adj[0].append(1)
-    adj[0].append(2)
-    adj[0].append(3)
-    adj[1].append(4)
-    adj[1].append(5)
-    adj[4].append(7)
-    adj[4].append(8)
-    adj[2].append(6)
-    adj[6].append(9)
+    gfg = GeeksforGeeks()
+    gfg.adj[0].append(1)
+    gfg.adj[0].append(2)
+    gfg.adj[0].append(3)
+    gfg.adj[1].append(4)
+    gfg.adj[1].append(5)
+    gfg.adj[4].append(7)
+    gfg.adj[4].append(8)
+    gfg.adj[2].append(6)
+    gfg.adj[6].append(9)
 
     # Calling dfs for node 0
     # Considering root node at 0
-    dfs(0, c)
+    gfg.dfs(0, c)
 
     # Print child nodes
-    Print(n)
+    gfg.Print(n)
 
 # This code is contributed by Rituraj Jain
