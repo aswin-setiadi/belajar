a3787b119a5260af064f22c66392fd003c9aef14
aswin-setiadi <aswin.setiadi@gmail.com>
2023-11-10 00:31:51 +0800

    add my version of subtreeofallnodes gfg answer

--- geeks4geeks/subtreeofallnodes.py
+++ geeks4geeks/subtreeofallnodes.py
@@ -4,6 +4,41 @@
 from typing import List
 
 
+class Solution:
+    def __init__(self, count: int) -> None:
+        self.node_count = count
+        self.start: list[int] = [0] * self.node_count
+        self.end: list[int] = [0] * self.node_count
+        self.dfs_nodes: list[int] = []
+        self.adj: list[list[int]] = [[] for _ in range(self.node_count)]
+        # assume 1 node has 1 parent, visited is not necessary
+        # self.visited: list[bool] = [False] * self.node_count
+
+    def add_node(self, node_index: int, node_child: int):
+        self.adj[node_index].append(node_child)
+
+    def dfs(self, index: int, dfs_nodes_index: int) -> int:
+        # +1 cause start from first child of this node to include in subtree
+        dfs_nodes_index += 1
+        self.start[index] = dfs_nodes_index
+        self.dfs_nodes.append(index)
+        for child in self.adj[index]:
+            dfs_nodes_index = self.dfs(child, dfs_nodes_index)
+        # dfs_nodes_index will be last node (bottomest rightmost)
+        # from this node. When this function in last traversed node, it will
+        # return +1  to index cause it is added by default
+        self.end[index] = dfs_nodes_index
+        return dfs_nodes_index
+
+    def print_subtrees(self):
+        for i in range(self.node_count):
+            if self.start[i] != self.end[i]:
+                print("subtree of node", i, "is", end=" ")
+                for j in range(self.start[i], self.end[i]):
+                    print(self.dfs_nodes[j], end=" ")
+                print()
+
+
 class GeeksforGeeks:
     def __init__(self) -> None:
         # Python3 code to print subtree of all nodes
@@ -51,8 +86,9 @@ class GeeksforGeeks:
                 print()
 
 
-# Driver code
-if __name__ == "__main__":
+def main_g4g():
+    # This code is contributed by Rituraj Jain
+    # Driver code
     # No of nodes n = 10
     n, c = 10, 0
     gfg = GeeksforGeeks()
@@ -73,4 +109,25 @@ if __name__ == "__main__":
     # Print child nodes
     gfg.Print(n)
 
-# This code is contributed by Rituraj Jain
+
+def main():
+    sol = Solution(10)
+    sol.add_node(0, 1)
+    sol.add_node(0, 2)
+    sol.add_node(0, 3)
+    sol.add_node(1, 4)
+    sol.add_node(1, 5)
+    sol.add_node(4, 7)
+    sol.add_node(4, 8)
+    sol.add_node(2, 6)
+    sol.add_node(6, 9)
+    sol.dfs(0, 0)
+    # print(sol.dfs_nodes)
+    # print(sol.start)
+    # print(sol.end)
+    sol.print_subtrees()
+
+
+if __name__ == "__main__":
+    main()
+    main_g4g()
