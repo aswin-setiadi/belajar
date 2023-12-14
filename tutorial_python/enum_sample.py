60988007ab77ef09cd3d9b960cc6c26a14f117af
aswin-setiadi <aswin.setiadi@gmail.com>
2023-12-14 21:36:49 +0800

    update enum sample tutorial

--- tutorial_python/enum_sample.py
+++ tutorial_python/enum_sample.py
@@ -15,12 +15,15 @@ def main():
     print(Shape.CIRCLE)  # 1
     print(Shape(1))  # 1
     print(Color.RED)  # Color.RED obj
-    print(Color(1))  # Color.RED
+    print(Color(1))  # Color.RED obj
     print(Color.RED.name)  # "RED" str
     print(Color.RED.value)  # 1 int
-    print(list(reversed(Color)))  # [Color.GREEN, Color.Red]
-    print(len(Shape))
-    print(Shape(3))  # ValueError
+    print(list(reversed(Color)))  # [Color.GREEN, Color.RED] intenum make order by value
+    print(len(Shape))  # 2
+    # print(Shape(3))  # ValueError
+    print(type(Color))  # EnumType
+    print(type(Color(1)))  # Color
+    print(type(Color.RED))  # Color
 
 
 if __name__ == "__main__":
