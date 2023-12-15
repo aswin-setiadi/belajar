fae5acf8d76522655a4962887d30e892a8a399e1
aswin-setiadi <aswin.setiadi@gmail.com>
2023-12-15 14:31:21 +0800

    leetcode medium 3 solution 2

--- leetcode/medium_3_longest_substring.py
+++ leetcode/medium_3_longest_substring.py
@@ -1,5 +1,8 @@
 #!/bin/python3
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+from collections import deque
+
+
 class Solution:
     # 5.78% 63.8%
     def lengthOfLongestSubstring(self, s: str) -> int:
@@ -30,10 +33,37 @@ class Solution:
         longest = max(longest, (end - start + 1))
         return longest
 
+    # 48.55% 29.44%
+    @staticmethod
+    def solve(s: str) -> int:
+        longest: int = 0
+        word: deque[str] = deque()
+        start = 0
+        for index in range(len(s)):
+            cur = s[index]
+            if cur in word:
+                longest = max(longest, index - start)
+                while True:
+                    if word[0] == cur:
+                        start += 1
+                        word.popleft()
+                        word.append(cur)
+                        break
+                    else:
+                        start += 1
+                        word.popleft()
+
+            else:
+                word.append(cur)
+        # need to take account the last
+        longest = max(longest, len(s) - start)
+        return longest
+
 
 if __name__ == "__main__":
-    ss = ["abcabcabc", "bbbbb", "abcabcbb", "  ", "", "tmmzuxt", "dvdf"]
+    ss = ["abcabcabc", "bbbb", "abcabcbb", "  ", "", "tmmzuxt", "dvdf"]
     anss = [3, 1, 3, 1, 0, 5, 3]
     for s in ss[:]:
-        ans = Solution().lengthOfLongestSubstring(s)
+        # ans = Solution().lengthOfLongestSubstring(s)
+        ans = Solution.solve(s)
         print(ans)
