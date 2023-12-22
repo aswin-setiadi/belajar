a6259de85ff7e87474733aa37d763d2e7667bbc4
aswin-setiadi <aswin.setiadi@gmail.com>
2023-12-22 15:15:19 +0800

    enum experiemtn

--- tutorial_python/enum_sample.py
+++ tutorial_python/enum_sample.py
@@ -4,11 +4,20 @@ from enum import Enum, IntEnum
 class Shape(IntEnum):
     CIRCLE = 1
     SQUARE = 2
+    RECTANGLE = 2
 
 
 class Color(Enum):
     RED = 1
     GREEN = 2
+    BLUE = 2
+
+
+class EnumDict(Enum):
+    First = {"a": 1}
+    Ten = {"a": 1}
+    Hundred = {"b": 1}
+    Thousand = {"a": 2}
 
 
 def main():
@@ -24,6 +33,18 @@ def main():
     print(type(Color))  # EnumType
     print(type(Color(1)))  # Color
     print(type(Color.RED))  # Color
+    print(Shape.RECTANGLE)  # 2
+    print(Color.GREEN)
+    print(Color.BLUE)  # Color.GREEN, blue will be alias of green (green declared first)
+    print(EnumDict.First)  # EnumDict.First
+    print(EnumDict.Ten)  # EnumDict.First, although different dict but dict content same
+    # warning says will always false cause they don't overlap but it print True
+    print(EnumDict.First == EnumDict.Ten)
+    print(EnumDict.First is EnumDict.Ten)  # True
+    print(EnumDict.First == EnumDict.Hundred)  # False
+    print(EnumDict.First is EnumDict.Hundred)  # False
+    print(EnumDict.First == EnumDict.Thousand)  # False
+    print(EnumDict.First is EnumDict.Thousand)  # False
 
 
 if __name__ == "__main__":
